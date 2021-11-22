ea7b559e92981715179cd47467e84d071a532077
Harshvardhan Sahu <hs7757@srmist.edu.in>
2021-11-22 12:18:08 +0530

    fixed button

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,8 +6,8 @@
 
 <div class="text-center p-10 ">
 
-    <h1 class="font-bold text-white text-7xl justify-center text-center py-16" >Chart. Chat. Trade. Repeat</h1>
+    <h1 class="font-bold text-white text-7xl justify-center text-center py-20" >Chart. Chat. Trade. Repeat</h1>
     
-    <a href = "/chart" class="text-white text-2xl justify-center text-center bg-bluea py-3 px-5 rounded-xl hover:bg-blueb hover:border-2 hover:border-bluea  hover:text-bluea hover:font-bold transition-all ">Start NOW!</a>
+    <a href = "/chart" class="text-white text-2xl justify-center text-center bg-bluea py-3 px-5 rounded-xl hover:bg-blueb hover:border-2 hover:border-textwhite  hover:text-textwhite  transition-all ">Start NOW!</a>
     <video  class="py-20"src="https://in.tradingview.com/static/bundles/1b46d7c30e364bde36e56bf139724b23.mp4" muted autoplay loop playsinline></video>
 </div>
